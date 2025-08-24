0de59b6a9560f6bc16e63e8abffda546568a44dd
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-24 17:07:11 -0400

    Update 88. Merge Sorted Array.py
    Space complexity: O(1)

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -7,26 +7,32 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        i = j = 0
-        nums3 = []
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                nums3.append(nums1[i])
-                i = i + 1
+        i = m - 1
+        j = n - 1
+        last = m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[last] = nums1[i]
+                i -= 1
+                last -= 1
             else:
-                nums3.append(nums2[j])
-                j = j + 1
-        nums3.extend(nums1[i:m])
-        nums3.extend(nums2[j:n])
-        nums1[:]= nums3
+                nums1[last] = nums2[j]
+                j -= 1
+                last -= 1
+        if i >= 0:
+            nums1[:last+1] = nums1[:i+1]
+        if j >= 0:
+            nums1[:last+1] = nums2[:j+1]
+
+
 
 if __name__ == "__main__":
-    nums1 = [1, 2, 3, 0, 0, 0]
-    m = 3
-    nums2 = [2, 5, 6]
-    n = 3
+    nums1 = [0]
+    m = 0
+    nums2 = [2]
+    n = 1
 
     sol = Solution()
     sol.merge(nums1, m, nums2, n)
 
-    # print("Result:", nums1)  # 期望输出: [1,2,2,3,5,6]
+    print("Result:", nums1)  # 期望输出: [1,2,2,3,5,6]
