770e28fe7c03e68cc5600bf3dc0ac11b16989486
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-25 22:35:23 -0400

    Update 169. Majority Element.py

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -4,18 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        showup = {}
-        for i in range(n):
-            if nums[i] in showup:          # 修正 showpup -> showup
-                showup[nums[i]] += 1
+        cand=None
+        cnt=0
+        for x in nums:
+            if cnt==0:
+                cand=x
+                cnt=1
+            elif x==cand:
+                cnt+=1
             else:
-                showup[nums[i]] = 1        # 修正语法：冒号 -> =，并且初始化为 1
-
-        max_count = -1                     # 避免覆盖 Python 内置函数 max
-        max_key = None
-        for key in showup:
-            if showup[key] > max_count:    # 修正访问方式
-                max_count = showup[key]
-                max_key = key
-        return max_key
+                cnt-=1
+        return cand
