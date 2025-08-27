19cb0ed4fda2df286a5d4a66a208639f8e682fb9
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-27 12:04:30 -0400

    Update 55. Jump Game.py

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -4,16 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        j = 0
-        dj = [nums[0]]
-
-        for i in range(1, len(nums)):
-            if i <= dj[j]:
-                if nums[i] + i > dj[j]:
-                    dj.append(nums[i] + i)
-                    j += 1
-
-        if dj[-1] >= len(nums) - 1:
-            return True
-        else:
-            return False
+        farthest=0
+        for i, step in enumerate(nums):
+            if i>farthest:
+                return False
+            farthest = max (farthest, i+step)
+        return True
