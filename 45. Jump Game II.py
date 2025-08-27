80fa86569ba3c5a040c336b1ff7767f74c9e3c38
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-27 12:20:54 -0400

    Update 45. Jump Game II.py

--- 45. Jump Game II.py
+++ 45. Jump Game II.py
@@ -4,14 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        step = 0
-        furthest = 0
-        cur_right = 0
+        n = len(nums)
 
-        for i in range(len(nums)):
-            if cur_right < i:
+        if n <= 1:
+            return 0
+
+        step = 0
+        cur_right = 0
+        furthest = 0
+
+        for i in range(n - 1):
+            furthest = max(furthest, nums[i] + i)
+            if cur_right == i:
                 step += 1
                 cur_right = furthest
-            furthest = max(furthest, nums[i] + i)
 
         return step
