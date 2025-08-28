54aee6eb935875a6277cadfa5ab42ab02596380d
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-27 20:34:12 -0400

    Update 238. Product of Array Except Self.py

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -5,22 +5,13 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        left, right = [1] * n, [1] * n
-
-        ans = [0] * n
-
+        ans = [1] * n
         for i in range(1, n):
-            left[i] = nums[i - 1] * left[i - 1]
+            ans[i] = nums[i - 1] * ans[i - 1]
 
+        R = nums[-1]
         for i in range(n - 2, -1, -1):
-            right[i] = nums[i + 1] * right[i + 1]
-
-        ans[0] = right[0]
-        ans[-1] = left[n - 1]
-
-        for i in range(1, n - 1):
-            ans[i] = left[i] * right[i]
+            ans[i] = ans[i] * R
+            R *= nums[i]
 
         return ans
-
-
