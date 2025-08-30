d6dbccfebdc4cd5b1c12b93c6f35d9ec2143d486
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-30 15:36:54 -0400

    Update 151. Reverse Words in a String.py

--- 151. Reverse Words in a String.py
+++ 151. Reverse Words in a String.py
@@ -4,13 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        array = s.strip().split()
-        l = 0
-        r = len(array) - 1
-
-        while l < r:
-            array[l], array[r] = array[r], array[l]
-            l += 1
-            r -= 1
-
-        return " ".join(array)
+        return " ".join(reversed(s.split()))
