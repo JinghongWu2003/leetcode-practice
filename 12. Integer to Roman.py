b1587c8ea263133324419719a24ab7754b344530
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-30 12:12:35 -0400

    Update 12. Integer to Roman.py

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -1,53 +1,14 @@
 class Solution(object):
     def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
-
-        def get_val(char):
-            if char == 'I':
-                return 1
-            elif char == 'V':
-                return 5
-            elif char == 'X':
-                return 10
-            elif char == 'L':
-                return 50
-            elif char == 'C':
-                return 100
-            elif char == 'D':
-                return 500
-            else:
-                return 1000
-
-        th = num / 1000
-        hun = num % 1000 / 100
-        ten = num % 100 / 10
-        sin = num % 10
-
-        ans = []
-
-        def judge(high, mid, low, num):
-            if num == 9:
-                ans.append(low + high)
-            elif num < 9 and num > 4:
-                ans.append(mid)
-                for i in range(num - 5):
-                    ans.append(low)
-            elif num == 4:
-                ans.append(low + mid)
-            else:
-                for i in range(num):
-                    ans.append(low)
-
-        for i in range(th):
-            ans.append('M')
-
-        judge('M', 'D', 'C', hun)
-        judge('C', 'L', 'X', ten)
-        judge('X', 'V', 'I', sin)
-
-        roman_str = "".join(ans)
-
-        return roman_str        
+        table = [(1000,'M'),(900,'CM'),(500,'D'),(400,'CD'),
+                 (100,'C'), (90,'XC'), (50,'L'), (40,'XL'),
+                 (10,'X'),  (9,'IX'),  (5,'V'),  (4,'IV'), (1,'I')]
+        res = []
+        append = res.append  # 微优化：减少属性查找
+        for v, s in table:
+            q, num = divmod(num, v)  # 一次得到商和余数
+            if q:
+                append(s * q)
+            if num == 0:             # 提前结束（通常能少走几步）
+                break
+        return ''.join(res)
