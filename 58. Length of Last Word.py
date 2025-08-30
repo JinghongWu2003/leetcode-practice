ad084cc4ae6b4606063a8741e7c0012ceb977ce5
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-08-30 12:17:19 -0400

    Create 58. Length of Last Word.py

---
+++ 58. Length of Last Word.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        cur = 0
+        flag = True
+        n = len(s)
+
+        for i in range(n):
+            if s[i] == ' ':
+                flag = True
+            elif flag and s[i] != ' ':
+                cur = 1
+                flag = False
+            else:
+                cur += 1
+
+        return cur
