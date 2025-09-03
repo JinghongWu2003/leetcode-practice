fc3eb993e700e3ccc1e02bc5d2b1a8b541358983
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-03 18:18:36 -0400

    Update 54. Spiral Matrix.py

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -1,28 +1,15 @@
 class Solution(object):
-    def spiralOrder(self, matrix):
+    def rotate(self, matrix):
         """
         :type matrix: List[List[int]]
-        :rtype: List[int]
+        :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        top, bottom = 0, len(matrix)-1
-        left, right = 0, len(matrix[0])-1
-        ans=[]
-        while top<=bottom and left<=right:
-            for i in range(left,right+1,1):
-                ans.append(matrix[top][i])
-            top+=1
+        n = len(matrix)
 
-            for i in range (top, bottom+1, 1):
-                ans.append(matrix[i][right])
-            right-=1
-
-            if top<=bottom:
-                for i in range(right,left-1,-1):
-                    ans.append(matrix[bottom][i])
-                bottom-=1
-            if left<=right:
-                for i in range (bottom , top-1, -1):
-                    ans.append(matrix[i][left])
-                left+=1
-
-        return ans 
+        for layer in range(n // 2):
+            first = layer
+            last = n - 1 - layer
+            for j in range(first, last):
+                offset = j - first
+                matrix[first][j], matrix[j][last], matrix[last][last - offset], matrix[last - offset][first] = \
+                    matrix[last - offset][first], matrix[first][j], matrix[j][last], matrix[last][last - offset]
