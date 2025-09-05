30bc35fecd327520f3b8d0ef5ff2c30ce335d886
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-05 10:30:05 -0400

    Update 383. Ransom Note.py

--- 383. Ransom Note.py
+++ 383. Ransom Note.py
@@ -1,17 +1,13 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
         if len(ransomNote) > len(magazine):
             return False
-        freq = {}
+        freq = [0]*26
         for ch in magazine:
-            freq[ch] = freq.get(ch, 0) + 1
+            freq[ord(ch)-97] += 1
         for ch in ransomNote:
-            if freq.get(ch, 0) == 0:
+            k = ord(ch)-97
+            freq[k] -= 1
+            if freq[k] < 0:
                 return False
-            freq[ch] -= 1
         return True
