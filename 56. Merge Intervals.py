727979ac08d25d075fd69afcdf921dd34e0180a6
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-07 15:43:19 -0400

    Update 56. Merge Intervals.py

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -1,37 +1,15 @@
 class Solution(object):
     def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-
-        intervals.sort()
-        start, end = intervals[0][0], intervals[0][1]
-        ans = [[start, end]]
-        j = 0
-        for i in range(1, len(intervals)):
-            # if intervals[i][0] <= end and intervals[i][1] >= start:
-            if (intervals[i][0] > end or intervals[i][1] < start):
-                start = intervals[i][0]
-                end = intervals[i][1]
-                ans.append([start, end])
-                j += 1
+        if not intervals:
+            return []
+        intervals.sort(key=lambda x: x[0])  # 只按左端点排
+        merged = []
+        for s, e in intervals:
+            if not merged or s > merged[-1][1]:
+                merged.append([s, e])
             else:
-                ans[j] = [min(start, intervals[i][0]), max(end, intervals[i][1])]
-                start = min(start, intervals[i][0])
-                end = max(end, intervals[i][1])
+                merged[-1][1] = max(merged[-1][1], e)
+        return merged
 
-        # start, end = ans[-1][0], ans[-1][1]
-        # i=len(ans)-2
-        # while i>=0:
-        #     if (ans[i][0] > end or ans[i][1] < start):
-        #         i-=1
-        #     else:
-        #         ans[i] = [min(start, ans[i][0]), max(end, ans[i][1])]
-        #         start = min(start, ans[i][0])
-        #         end = max(end, ans[i][1])
-        #         del ans[i]
-        #         i-=1
-        return ans
-
-print(Solution().merge([[2,3],[5,5],[2,2],[3,4],[3,4]]))
+# 测试
+print(Solution().merge([[2,3],[5,5],[2,2],[3,4],[3,4]]))  # -> [[2,4],[5,5]]
