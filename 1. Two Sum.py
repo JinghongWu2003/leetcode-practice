52195a131466c55d63823a238825d8bd11ceb42a
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-07 11:29:33 -0400

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -5,28 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        sort_nums = sorted(nums)
+        seen={}
 
-        i, j = 0, len(nums)-1
-
-        a,b=0,0
-
-        while i < j:
-            if sort_nums[i]+sort_nums[j]==target:
-                a=sort_nums[i]
-                b=sort_nums[j]
-                break
-            elif sort_nums[i]+sort_nums[j] < target:
-                i+=1
-            else:
-                j-=1
-
-        ans=[]
-        for i in range(len(nums)):
-            if nums[i]==a:
-                ans.append(i)
-            elif nums[i]==b:
-                ans.append(i)
-        return ans
-
-print(Solution().twoSum([-3,4,3,90],0))
+        for i, x in enumerate(nums):
+            y = target - x
+            if y in seen:
+                return [seen[y],i]
+            seen[x]=i
+        return []
