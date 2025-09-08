ce70ec8332fe2ca860b49d205ace1fbefb92a7c3
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-08 14:41:59 -0400

    Update 155. Min Stack.py

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -1,52 +1,21 @@
-from collections import defaultdict
-
 class MinStack(object):
     def __init__(self):
         self.stack = []
-        self.topindex = -1
-        self.minVal = defaultdict(int)
+        self.min_stack = []
 
     def push(self, val):
         self.stack.append(val)
-        self.topindex = len(self.stack) - 1
-        if self.topindex == 0:
-            self.minVal[self.topindex] = val
-        else:
-            self.minVal[self.topindex] = min(self.minVal[self.topindex - 1], val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
 
     def pop(self):
         if self.stack:
-            self.stack.pop()
-            self.topindex = len(self.stack) - 1
+            val = self.stack.pop()
+            if val == self.min_stack[-1]:
+                self.min_stack.pop()
 
     def top(self):
-        return self.stack[self.topindex]
+        return self.stack[-1]
 
     def getMin(self):
-        return self.minVal[self.topindex]
-
-
-# ---------------- 本地驱动测试 ----------------
-if __name__ == "__main__":
-    ops = ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
-    params = [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
-
-    obj = None
-    results = []
-
-    for op, p in zip(ops, params):
-        if op == "MinStack":
-            obj = MinStack()
-            results.append(None)
-        elif op == "push":
-            obj.push(p[0])
-            results.append(None)
-        elif op == "pop":
-            obj.pop()
-            results.append(None)
-        elif op == "top":
-            results.append(obj.top())
-        elif op == "getMin":
-            results.append(obj.getMin())
-
-        print(results)
+        return self.min_stack[-1]
