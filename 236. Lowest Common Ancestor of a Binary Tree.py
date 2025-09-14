0eb6d705acc8996dbf2bd7107273e1904f5e0926
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-14 16:30:08 -0400

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- 236. Lowest Common Ancestor of a Binary Tree.py
+++ 236. Lowest Common Ancestor of a Binary Tree.py
@@ -7,27 +7,12 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.parent_map = {}
+        if not root or root == q or root == p:
+            return root
 
-        def dfs(node):
-            if not node:
-                return
-            if node.left:
-                self.parent_map[node.left] = node
-                dfs(node.left)
-            if node.right:
-                self.parent_map[node.right] = node
-                dfs(node.right)
+        left = self.lowestCommonAncestor(root.left, q, p)
+        right = self.lowestCommonAncestor(root.right, q, p)
 
-        dfs(root)
-        visited = set()
-        cur = p
-        while cur:
-            visited.add(cur)
-            cur = self.parent_map.get(cur)
-        cur = q
-
-        while cur:
-            if cur in visited:
-                return cur
-            cur = self.parent_map.get(cur)
+        if left and right:
+            return root
+        return left if left else right
