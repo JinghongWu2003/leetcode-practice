b1e245854031989ddf513bb6dc92f4324bce76ca
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-13 20:18:50 -0400

    Update 112. Path Sum.py

--- 112. Path Sum.py
+++ 112. Path Sum.py
@@ -8,18 +8,7 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-        self.flag=False
-        def dfs(root: Optional[TreeNode], target: int) -> bool:
-            if not root:
-                return False
-            if not root.left and not root.right:
-                if root.val==target:
-                    self.flag=True
-            if root.left:
-                root.left.val+=root.val
-                dfs(root.left,target)
-            if root.right:
-                root.right.val+=root.val
-                dfs(root.right,target)
-        dfs(root, targetSum)
-        return self.flag
+        if not root.left and not root.right:
+            return root.val == targetSum
+
+        return (self.hasPathSum(root.left, targetSum - root.val)) or (self.hasPathSum(root.right, targetSum - root.val))
