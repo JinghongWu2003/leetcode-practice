b277c535df717d81b3b872eebcc7213c826b48cf
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-16 22:14:26 -0400

    Update 207. Course Schedule.py

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -1,59 +1,24 @@
-class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        for a, b in prerequisites:
+            graph[b].append(a)
 
+        visited = [0] * numCourses
 
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
+        def dfs(node):
+            if visited[node] == 1:
+                return False
+            if visited[node] == 2:
+                return True
+            visited[node] = 1
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+            visited[node] = 2
+            return True
 
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def _move_to_head(self, node):
-        self._remove(node)
-        self._add(node)
-
-    def _add(self, node):
-        nxt = self.head.next
-        self.head.next = node
-        node.prev = self.head
-        node.next = nxt
-        nxt.prev = node
-
-    def _remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        node = self.cache[key]
-        self._move_to_head(node)
-        return node.val
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
-            node.val = value
-            self._move_to_head(node)
-        else:
-            node = Node(key, value)
-            self.cache[key] = node
-            self._add(node)
-            if len(self.cache) > self.cap:
-                lru = self.tail.prev
-                self._remove(lru)
-                del self.cache[lru.key]
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
