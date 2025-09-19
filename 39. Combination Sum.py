ba796921bc5617e2eddf29d9bacc205708096f83
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-19 10:01:30 -0400

    Create 39. Combination Sum.py

---
+++ 39. Combination Sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res, path=[],[]
+        candidates.sort()
+        def dfs(start, remain):
+            if remain == 0:
+                res.append(path[:])
+                return
+            for i in range(start, len(candidates)):
+                x=candidates[i]
+                if x>remain:
+                    break
+                path.append(x)
+                dfs(i,remain-x)
+                path.pop()
+
+        dfs(0,target)
+        return res
