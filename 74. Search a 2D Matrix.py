73dc0f28cdcefaa39886e01ceb9a2b13eed0cbbf
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-19 18:01:00 -0400

    Update 74. Search a 2D Matrix.py

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -1,12 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row, col=len(matrix), len(matrix[0])
-        for i in range(row):
-            if matrix[i][0]<=target<=matrix[i][col-1]:
-                for j in range(col):
-                    if matrix[i][j]==target:
-                        return True
-                return False
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        top, bot = 0, m - 1
+        row = -1
+        while top <= bot:
+            mid = (top + bot) // 2
+            if matrix[mid][0] <= target <= matrix[mid][n - 1]:
+                row = mid
+                break
+            if target < matrix[mid][0]:
+                bot = mid - 1
             else:
-                continue
+                top = mid + 1
+        if row < 0:
+            return False
+        l, r = 0, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[row][mid] == target:
+                return True
+            if matrix[row][mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
         return False
+
