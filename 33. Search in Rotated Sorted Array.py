e583274cb7c1a3be7b9835f820caed279a2fc60f
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-20 13:54:41 -0400

    Update 33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -1,16 +1,18 @@
-from collections import defaultdict
-
-
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        groups = defaultdict(list)
-        for x in strs:
-            cnt=[0]*26
-            for s in x:
-                cnt[ord(s) - ord('a')] += 1
-            groups[tuple(cnt)].append(x)
-        return list(groups.values())
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r=0, len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid] ==target:
+                return mid
+            if nums[l]<=nums[mid]:
+                if nums[l]<=target<=nums[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
+            else:
+                if nums[mid]<target<=nums[r]:
+                    l=mid+1
+                else:
+                    r=mid-1
+        return -1
