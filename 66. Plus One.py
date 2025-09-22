f061bf175511b7f1c14d979aa8a802abd5f02aad
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-22 15:05:26 -0400

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -1,10 +1,5 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry=1
-        for i in range(len(digits)-1,-1,-1):
-            digits[i]+=carry
-            carry=digits[i]//10
-            digits[i]=digits[i]%10
-        if carry:
-            digits.insert(0,1)
-        return digits
+        num=int("".join(map(str,digits)))
+        num+=1
+        return [int(ch) for ch in str(num)]
