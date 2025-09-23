7a0ab1b03076e3b4402401f9b630581c5a5d75f0
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-23 00:50:12 -0400

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -4,14 +4,14 @@ from typing import List
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[1]*(n+1)
+        dp=[1]*(n)
         best=1
         # dp[0]=0
         for i in range(1, n):
             for j in range(i):
                 if nums[j]< nums[i]:
                     dp[i]=max(dp[i],dp[j]+1)
-                    best=max(dp[i],best)
+            best=max(dp[i],best)
         return best
 
 print(Solution().lengthOfLIS([10,9,2,5,3,7,101,18]))
