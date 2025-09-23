ee26d0532d0531b1d650182310b87204408ea30e
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-22 20:04:58 -0400

    Create 69. Sqrt(x).py

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2:
+            return x
+        l,r=0,x//2+1
+        ans=1
+        while l<=r:
+            mid=(l+r)//2
+            sq=mid*mid
+            if sq==x:
+                return mid
+            if sq<x:
+                ans=mid
+                l=mid+1
+            else:
+                r=mid-1
+        return ans
