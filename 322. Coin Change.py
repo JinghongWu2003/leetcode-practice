5f9b6a1f088c2b2e7310decdd922c33a070a0f31
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-22 22:52:30 -0400

    Update 322. Coin Change.py

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,16 +1,10 @@
-from typing import List
-
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp=[-1]*(amount+1)
+        INF=amount+1
+        dp=[INF]*(amount+1)
         dp[0]=0
         for i in range(1,amount+1):
-            imin=10001
             for x in coins:
-                if i-x>=0 and dp[i-x]!=-1:
-                    imin=min(imin,dp[i-x])
-            dp[i]=imin+1 if imin!=10001 else -1
-        return dp[amount]
-
-print(Solution().coinChange([1,2,5], 11))
+                if i>=x:
+                    dp[i]=min(dp[i],dp[i-x]+1)
+        return -1 if dp[amount]==INF else dp[amount]
