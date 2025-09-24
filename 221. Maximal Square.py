a96c333f5c55d24820e450f208f23b5fefaed0ea
Jinghong Wu <90080572+JinghongWu2003@users.noreply.github.com>
2025-09-24 00:48:28 -0400

    Create 221. Maximal Square.py

---
+++ 221. Maximal Square.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, grid: List[List[str]]) -> int:
+        best = 0
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            if grid[i][0] == '1':
+                dp[i][0] =1
+            else:
+                dp[i][0] = 0
+        for i in range(n):
+            if grid[0][i] == '1':
+                dp[0][i] = 1
+            else:
+                dp[0][i] = 0
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if grid[i][j] == '0':
+                    dp[i][j] = 0
+                else:
+                    if dp[i - 1][j - 1] > 0 and dp[i][j - 1] > 0 and dp[i - 1][j] > 0:
+                        if dp[i - 1][j - 1] == dp[i][j - 1] == dp[i - 1][j]:
+                            dp[i][j] = dp[i - 1][j - 1] + 1
+                        else:
+                            dp[i][j] = max(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j])
+                    else:
+                        dp[i][j] = 1
+                best = max(best, dp[i][j])
+        return best * best
+
+print(Solution().maximalSquare([["1","1","1","1","0"],["1","1","1","1","0"],["1","1","1","1","1"],["1","1","1","1","1"],["0","0","1","1","1"]]))
